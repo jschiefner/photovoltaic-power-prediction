cb3ea5fa90f0c1e920537a0c8085035e8d1f36fc
Jonas Schiefner <jonas.schiefner@zweitag.de>
2019-07-29 14:50:19 +0200

    implement pvwatts api_import functionality

---
+++ importers/pvwatts.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd
+import requests
+
+def api_import(system_capacity=4, module_type=0,
+            losses=14, array_type=1, tilt=25, azimuth=180,
+            address=None, lat=51.9607, lon=7.6261, radius=0):
+    """
+    Imports data from PVWatts using the pypvwatts package.
+    Only fields that are of importance for this forecasting purpose
+    can be specified.
+    """
+    api_key = os.environ['PVWATTS_API_KEY']
+    params = {
+        'format': 'json',
+        'api_key': api_key,
+        'system_capacity': system_capacity,
+        'module_type': module_type,
+        'losses': losses,
+        'array_type': array_type,
+        'tilt': tilt,
+        'azimuth': azimuth,
+        'address': address,
+        'lat': lat,
+        'lon': lon,
+        'radius': radius,
+        'timeframe': 'hourly'
+    }
+
+    response = requests.get('https://developer.nrel.gov/api/pvwatts/v6.json', params)
+    response.raise_for_status()
+    outputs = response.json()['outputs']
+
+    data = {key: outputs[key] for key in ['ac', 'tamb', 'wspd']}
+    data['power'] = data.pop('ac')
+    data = pd.DataFrame(data)
+    data['time'] = pd.date_range('20190101', periods=len(data), freq='H')
+    data.set_index('time', inplace=True)
+    return data
