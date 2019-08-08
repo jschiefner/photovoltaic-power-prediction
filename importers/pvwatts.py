7f55cfa301e457e102489d79bef83a8b084144fc
Jonas Schiefner <jonas.schiefner@zweitag.de>
2019-08-08 02:01:01 +0200

    restructue pvwatts importer and add load_from_json method

--- importers/pvwatts.py
+++ importers/pvwatts.py
@@ -1,10 +1,22 @@
 import os
 import pandas as pd
 import requests
+import json
+import warnings
 
 API_KEY = os.environ['PVWATTS_API_KEY']
 
-def load(system_capacity=4, module_type=0, losses=14, array_type=0, tilt=25, azimuth=180, address=None, lat=51.9607, lon=7.6261, radius=0):
+def _json_to_dataframe(json):
+    outputs = json['outputs']
+    data = {key: outputs[key] for key in ['ac', 'tamb', 'wspd']}
+    data['power'] = data.pop('ac')
+    data = pd.DataFrame(data)
+    data['time'] = pd.date_range('20190101', periods=len(data), freq='H')
+    data.set_index('time', inplace=True)
+    return data
+
+warnings.simplefilter('always')
+def load(system_capacity=4, module_type=0, losses=14, array_type=0, tilt=25, azimuth=180, address=None, lat=51.9607, lon=7.6261, radius=100, dataset='intl', suppress_warnings=False):
     """
     Imports data from PVWatts using the requests package.
     Only fields that are of importance for this forecasting purpose
@@ -23,19 +35,28 @@ def load(system_capacity=4, module_type=0, losses=14, array_type=0, tilt=25, azi
         'lon': lon,
         'radius': radius,
         'timeframe': 'hourly',
-        'dataset': 'tmy3'
+        'dataset': dataset
     }
 
     response = requests.get('https://developer.nrel.gov/api/pvwatts/v6.json', params)
+    json = response.json()
+    if not suppress_warnings:
+        if json['errors']: warnings.warn(f'API ERROR: {json["errors"]}')
+        if json['warnings']: warnings.warn(f'API WARNING: {json["warnings"]}')
     response.raise_for_status()
-    outputs = response.json()['outputs']
+    print(f"loaded {json['station_info']['city']}")
+    return _json_to_dataframe(json)
 
-    data = {key: outputs[key] for key in ['ac', 'tamb', 'wspd']}
-    data['power'] = data.pop('ac')
-    data = pd.DataFrame(data)
-    data['time'] = pd.date_range('20190101', periods=len(data), freq='H')
-    data.set_index('time', inplace=True)
-    return data
+def load_from_json(filepath):
+    """
+    Imports PVWatts data that has been downloaded and stored in a json file
+
+    filepath: str. path to a json file containing a valid json document
+                   as specified by the PVWatts API.
+    """
+    with open(filepath) as file:
+        data = json.load(file)
+    return _json_to_dataframe(data)
 
 def bulk_load_from_list(filepath, range=None):
     """
@@ -50,7 +71,10 @@ def bulk_load_from_list(filepath, range=None):
     list = list[start:stop]
     cities = {}
     for index, (city, lat, lon) in enumerate(list.values):
-        cities[city] = load(lat=lat, lon=lon)
+        try:
+            cities[city] = load(lat=lat, lon=lon, suppress_warnings=True)
+        except requests.HTTPError as e:
+            cities[city] = load(lat=lat, lon=lon, dataset='tmy3')
     return cities
 
 def load_city_from_list(filepath, city):
@@ -62,4 +86,8 @@ def load_city_from_list(filepath, city):
     """
     list = pd.read_csv(filepath).set_index('city')
     city = list.loc[city]
-    return load(lat=city.lat, lon=city.lon)
+    try:
+        result = load(lat=city.lat, lon=city.lon, suppress_warnings=True)
+    except requests.HTTPError as e:
+        result = load(lat=city.lat, lon=city.lon, dataset='tmy3')
+    return result
