29d033055613c19471dcc47caf65b519c50188a6
Jonas Schiefner <jonas.schiefner@zweitag.de>
2019-08-08 18:13:40 +0200

    round error values in error_terms module

--- evaluation/error_terms.py
+++ evaluation/error_terms.py
@@ -9,7 +9,7 @@ def mse(y_true, y_pred):
     y_true: series. Expected values
     y_pred: series. Predicted values
     """
-    return mean_squared_error(y_true, y_pred)
+    return round(mean_squared_error(y_true, y_pred), 2)
 
 def rmse(y_true, y_pred):
     """
@@ -20,7 +20,7 @@ def rmse(y_true, y_pred):
     y_pred: series. Predicted values
     """
     output = mse(y_true, y_pred)
-    return sqrt(output)
+    return round(sqrt(output), 2)
 
 def nrmse(y_true, y_pred):
     """
@@ -31,4 +31,4 @@ def nrmse(y_true, y_pred):
     y_pred: series. Predicted values
     """
     output = rmse(y_true, y_pred)
-    return output / y_true.mean()
+    return round(output / y_true.mean(), 2)
