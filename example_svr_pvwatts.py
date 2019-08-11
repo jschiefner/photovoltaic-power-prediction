b86c75223f2116821d7889b09d980089ae805232
Jonas Schiefner <jonas.schiefner@zweitag.de>
2019-08-12 01:26:07 +0200

    adjust svr examples

--- example_svr_pvwatts.py
+++ example_svr_pvwatts.py
@@ -1,15 +1,15 @@
 from importers import pvwatts
 from predictors.svr_model import SVRModel
-from evaluation.error_terms import mse, rmse, nrmse
+from evaluation.error_terms import mse, rmse, nrmse, r2
 import matplotlib.pyplot as plt
 from pandas.plotting import register_matplotlib_converters; register_matplotlib_converters() # fix pandas future warnings
 
 # load data
-data = pw.load(lat=52.470001220703125, lon=13.399999618530273) # station in berlin
+data = pvwatts.load_city_from_list('data/pvwatts/stations_list.csv', 'Berlin')
 
 # prepare testing and training data
-training = data['20190604':'20190630'] # use 4 weeks in january for training
-testing = data['20190701':'20190702'] # use 2 days in february for testing
+training = data['20190604':'20190630'] # use 4 weeks in june for training
+testing = data['20190701':'20190702'] # use 2 days in july for testing
 
 # prepare model
 model = SVRModel(base_data=data, scaling=True)
@@ -20,6 +20,7 @@ model.predict(testing)
 print(f'MSE: {mse(testing.power, model.prediction.power)}')
 print(f'RMSE: {rmse(testing.power, model.prediction.power)}')
 print(f'nRMSE: {nrmse(testing.power, model.prediction.power)}')
+print(f'R2: {r2(testing.power, model.prediction.power)}')
 
 # plot testing features
 plt.plot(testing.tamb, color='red')
--- example_svr_uq.py
+++ example_svr_uq.py
@@ -1,14 +1,13 @@
 from importers import uq
 from predictors.svr_model import SVRModel
-from evaluation.error_terms import mse, rmse, nrmse
+from evaluation.error_terms import mse, rmse, nrmse, r2
 import matplotlib.pyplot as plt
 from pandas.plotting import register_matplotlib_converters; register_matplotlib_converters() # fix pandas future warnings
 
 # load data
-data = uq.load('power.csv', 'weather.csv')
+data = uq.load('data/uq/power/car_park_1/2014.csv', 'data/uq/weather/2014.csv')
 
 # prepare testing and training data
-# make sure the date ranges fit your data
 training = data['20140104':'20140131'] # use 4 weeks in january for training
 testing = data['20140201':'20140202'] # use 2 days in february for testing
 
@@ -21,6 +20,7 @@ model.predict(testing)
 print(f'MSE: {mse(testing.power, model.prediction.power)}')
 print(f'RMSE: {rmse(testing.power, model.prediction.power)}')
 print(f'nRMSE: {nrmse(testing.power, model.prediction.power)}')
+print(f'R2: {r2(testing.power, model.prediction.power)}')
 
 # plot testing features
 plt.plot(testing.airtemp, color='red')
